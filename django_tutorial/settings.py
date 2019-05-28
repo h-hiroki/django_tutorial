9dcb36615da5f1d0a26351205324a79a4339df18
h-hiroki <gyouzakuuka@gmail.com>
2019-05-28 20:44:41 +0900

    ⛳️その２やった
    https://docs.djangoproject.com/ja/2.2/intro/tutorial02/

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_tutorial',
-        'USER': 'root'
+        'USER': 'root',
     }
 }
 
